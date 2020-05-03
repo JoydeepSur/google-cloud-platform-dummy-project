cdf089889107b23d2aac0c7d88ae5122f3dc1c53
Joydeep Sur <joydeepsur6@gmail.com>
2020-05-03 11:28:14 +0530

    initial commit

---
+++ src/main/java/com/example/google/GoogleCloudPlatformDummyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudPlatformDummyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudPlatformDummyProjectApplication.class, args);
+	}
+
+}
